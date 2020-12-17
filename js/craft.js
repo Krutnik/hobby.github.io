b89ead09b1eddfae5f1b940a0a5590de1b4f1108
Кирилл <67455038+Krutnik@users.noreply.github.com>
2020-12-17 14:20:23 +0300

    update

--- js/craft.js
+++ js/craft.js
@@ -2,13 +2,16 @@
 let btnNewWindow = document.querySelector('.head__btn');
 let hidden = document.querySelector('.hidden');
 let test;
+let main = document.getElementById('main')
 
 
 btnNewWindow.onclick = function () {
     if(hidden.classList.contains('hidden')) {
         hidden.classList.remove('hidden');
+        document.body.classList.add('opacity');
     }
     else {
         hidden.classList.add('hidden');
+        document.body.classList.remove('opacity');
     }
 }
